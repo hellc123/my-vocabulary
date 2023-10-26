e91569fdd10b607607328e2caf3eb4efa658efa6
Jia Li <1437050729@qq.com>
2023-10-27 00:49:35 +0800

    delete old code

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,6 @@
 #include<iostream>
-#include"fileIO.h"
-#include"wordMap.h"
-#include"wordProcess.h"
 int main(int argc, char* argv[])
 {
 
-	//char dir1[]{ "./123.txt" };
-	//char dir2[]{ "./iotest.exe" };
-	//fileI fin(dir1);
-	//auto buff = fin.toSStream();
-	//if (buff)
-	//{
-	//	wordMap wmap;
-	//	wordProcess(*buff, wmap);
-	//	fileO fout(dir1, wmap.output());
-	//}
-	//else
-	//{
-	//	std::cout << "NULL\n";
-	//}
-
-	for (int i = 1; i < argc; i++)
-	{
-		fileI fin(argv[i]);
-		auto buff = fin.toSStream();
-		if (buff)
-		{
-			wordMap wmap;
-			wordProcess(*buff, wmap);
-			fileO fout(argv[i], wmap.output());
-		}
-		else
-		{
-			std::cout << "NULL\n";
-		}
-	}
-
-	//system("pause");
 	return 0;
 }
